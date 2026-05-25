d564a78f77ac3aab327f0acb55930fca9930348e
Seekinward <1441739534@qq.com>
2026-05-25 11:59:18 +0800

    Initial commit

---
+++ src/main/java/com/recommendation/intelligentoutfitrecommendationsystem/IntelligentOutfitRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.recommendation.intelligentoutfitrecommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentOutfitRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentOutfitRecommendationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recommendation/intelligentoutfitrecommendationsystem/IntelligentOutfitRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommendation.intelligentoutfitrecommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentOutfitRecommendationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
